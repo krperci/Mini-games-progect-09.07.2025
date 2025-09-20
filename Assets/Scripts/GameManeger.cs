d0a2d2409cdd85e84d0abdb4baf6a4f72e2ce0d7
krperci <krperci@gmail.com>
2025-09-20 20:31:17 +0300

    майже завершив хрестики нулики

--- Assets/Scripts/GameManeger.cs
+++ Assets/Scripts/GameManeger.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,32 +9,86 @@ using UnityEngine.UI;
 public class GameManeger : MonoBehaviour
 {
     public Sprite xImage;
+    public Sprite defImage;
+    int winner;
+    public TextMeshProUGUI winertext;
     public Sprite oImage;
-    public bool xTurn = true;
+    int[] board = new int[9];
+    public int currentPlayer = 1;
+    readonly int[][] winLines = new int[][]
+{
+    new int[]{0,1,2},
+    new int[]{3,4,5},
+    new int[]{6,7,8},
+    new int[]{0,3,6},
+    new int[]{1,4,7},
+    new int[]{2,5,8},
+    new int[]{0,4,8},
+    new int[]{2,4,6}
+
+
+};
 
     public Button[] buttons;
     public void MakeMove(int index) 
     {
-        if (xTurn == true)
+        board[index] = currentPlayer;
+        if (currentPlayer == 1)
         {
             buttons[index].GetComponent<Image>().sprite = xImage;
-            xTurn = false;
         }
         else 
         {
             buttons[index].GetComponent<Image>().sprite = oImage;
-            xTurn=true;  
         }
-        Check();
-
-    }
-    public void Check() 
-    {
-        if (true) 
+        buttons[index].interactable = false;
+        winner = Check();
+        currentPlayer *= -1;
+        if (winner != 0)
         {
-            
+            if (winner == 1)
+            {
+                winertext.text = "Хрестики перемогли";
+            }
+            else if (winner == -1)
+            {
+                winertext.text = "Нулики перемогли";
+            }
+            else
+            {
+                winertext.text = "Нічия";
+
+            }
+            for (int i = 0; i < 9; i++)
+                buttons[i].interactable = false;
+            return;
         }
     }
+    public int Check() 
+    {
+        foreach (var line in winLines)
+        {
+            int a = board[line[0]];
+            if (a != 0 && a == board[line[1]] && a == board[line[2]])
+            {
+                return a;
+            }
+        }
+        return 0;
+    }
+    public void InitBoard()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            board[i] = 0;
+            int index = i;
+            buttons[index].GetComponent<Image>().sprite = defImage;
+
+            buttons[i].interactable = true;
+        }
+        currentPlayer = 1;
+    }
+
     public void EndGame() 
     {
               
